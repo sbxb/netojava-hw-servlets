5ab47381fdec2ed2843620276e998cdd9c1fcc29
Vadim Persidskii <vadim@example.com>
2025-01-25 16:13:46 +0300

    Implement PostRepository

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,29 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-// Stub
 public class PostRepository {
+    private final Map<Long, Post> repo = new HashMap<>();
+
     public List<Post> all() {
-        return Collections.emptyList();
+        return new ArrayList<>(repo.values());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        return Optional.ofNullable(repo.get(id));
     }
 
     public Post save(Post post) {
+        repo.put(post.getId(), post);
         return post;
     }
 
     public void removeById(long id) {
+        repo.remove(id);
     }
 }
